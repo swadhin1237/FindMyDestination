f981fe0eea5f403abce6d20d2c39d5c886c1bd0f
Crystal11101 <janmaybatra@gmail.com>
2023-11-04 12:50:12 +0530

    Initial commit

--- project.py
+++ project.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import geocoder
+from geopy.geocoders import Nominatim
+from geopy.point import Point
 import folium
-
-
 
 def loca():
     g=geocoder.ip("me")
     myAddr=g.latlng
+    geolocator=Nominatim(user_agent="myGeocoder")
+    loc=f'{myAddr[0]} , {myAddr[1]}'
+    locname=geolocator.reverse(loc)
+    print(locname.addr)
     return myAddr
 
+def getImage(location_name):
+    pass
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,6 +25,15 @@ def index():
 def location():
     addr=loca()
     return addr
+
+@app.route('/map')
+def map():
+    addr=loca()
+    map = folium.Map(location=addr, zoom_start=12)
+    print(addr)
+    folium.Marker(addr, popup='My Location').add_to(map)
+    map.save('templates/map.html')
+    return render_template('map.html')
 
 app.run(debug=True)
 
